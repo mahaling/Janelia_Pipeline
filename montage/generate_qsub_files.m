2bd85662778307cc96f6c3643f558fd7180847ef
Gayathri Mahalingam <mahalingamg@uncw.edu>
2017-03-16 13:19:52 -0700

    adding qsub files

--- montage/generate_qsub_files.m
+++ montage/generate_qsub_files.m
@@ -4,7 +4,7 @@ str1 = '#!/bin/bash';
 log_file = ['log_' num2str(z) '.txt'];
 jsonfile = [num2str(z) '.json'];
 jobname = ['m_' num2str(z)];
-cmd = ['qsub -l nodes=12:ppn=20 -N ' jobname ' -q connectome -o /data/nc-em2/gayathrim/Janelia_Pipeline/montage/scratch/logs/' log_file ' -V batch 16 "/data/nc-em2/gayathrim/Janelia_Pipeline/montage/solve_montage_SL /data/nc-em2/gayathrim/Janelia_Pipeline/scratch/' jsonfile '"'];
+cmd = ['qsub -l nodes=12:ppn=20 -N ' jobname ' -q emconnectome -o /data/nc-em2/gayathrim/Janelia_Pipeline/montage/scratch/logs/' log_file ' -V batch 16 "/data/nc-em2/gayathrim/Janelia_Pipeline/montage/solve_montage_SL /data/nc-em2/gayathrim/Janelia_Pipeline/scratch/' jsonfile '"'];
 qsubfile = ['/data/nc-em2/gayathrim/Janelia_Pipeline/scratch/qsub_files/qsub_job_' num2str(z) '.sh'];
 fp = fopen(qsubfile, 'w');
 fprintf(fp, '%s\n', str1);
