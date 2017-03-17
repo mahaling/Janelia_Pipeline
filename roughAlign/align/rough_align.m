3bb1895b78483cc8d3fc73f5eeb60d14101ffa70
Gayathri Mahalingam <mahalingamg@uncw.edu>
2017-03-17 10:05:47 -0700

    rough align

--- roughAlign/align/rough_align.m
+++ roughAlign/align/rough_align.m
@@ -8,7 +8,7 @@ if isstruct(argin)
     sl = argin;
 else
     fn = argin;
-    sl = loadjson(filread(fn));
+    sl = loadjson(fileread(fn));
 end
 
 if sl.verbose,
@@ -27,14 +27,14 @@ if sl.filter_option == 1
 elseif sl.filter_option == 2
     sl.montage_scape_pm_generation.script = [sl.EM_aligner_path '/renderer_api/generate_montage_scape_point_matches_box_filter_no.sh'];
 else
-    sl.montage_scape_pm_generation.script = [sl.EM_aligner_path '/renderer_api/generate_montage_scape_point_matches_box.sh'];
+    sl.montage_scape_pm_generation.script = [sl.EM_aligner_path '/renderer_api/generate_montage_scape_point_matches.sh'];
 end
 
 if ~exist(sl.dir_store_rough_slab, 'dir')
     mkdir(sl.dir_store_rough_slab);
 end
-kk_mkdir(sl.montage_scape_pm_generation.base_output_dir); % this has to be created first as it is the upper level directory
-kk_mkdir(sl.dir_rough_intermediate_store); % purge and make new empty
+%kk_mkdir(sl.montage_scape_pm_generation.base_output_dir); % this has to be created first as it is the upper level directory
+%kk_mkdir(sl.dir_rough_intermediate_store); % purge and make new empty
 
 [L2, needs_correction, pmfn, zsetd, zrange, t, dir_spark_work, cmd_str, fn_ids, ...
     target_solver_path, target_ids, target_matches, target_layer_images]= ...
